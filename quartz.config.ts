d39c2be7fa281a1182846d57d38373b2d74f290d
apollo <dabido1@proton.me>
2026-01-31 12:34:55 +1100

    Quartz sync: Jan 31, 2026, 12:34 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -29,8 +29,8 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
-          lightgray: "#e5e5e5",
+          light: "#f1f1fc",
+          lightgray: "#fafafc",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
           dark: "#2b2b2b",
@@ -40,8 +40,8 @@ const config: QuartzConfig = {
           textHighlight: "#fff23688",
         },
         darkMode: {
-          light: "#161618",
-          lightgray: "#393639",
+          light: "#2f2d2e",
+          lightgray: "#262630",
           gray: "#646464",
           darkgray: "#d4d4d4",
           dark: "#ebebec",
